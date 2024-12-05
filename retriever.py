5f8e6f76079d62604805b91d6be8a052e1e9e01f
tufimtseva <tetiana.ufimtseva.kn.2021@lpnu.ua>
2024-12-04 23:49:00 -0500

    added BM25Okapi retriever, checkbox for searching by keyword, updated chat ui and reformatted classes

---
+++ retriever.py
@@ -0,0 +1,18 @@
+from rank_bm25 import BM25Okapi
+import numpy as np
+from lib.utils import tokenize
+
+
+class Retriever:
+    def __init__(self, docs: list[str]):
+        self.docs = docs
+        tokenized_docs = [tokenize(doc) for doc in self.docs]
+        self.bm25 = BM25Okapi(tokenized_docs)
+
+    def get_relevant_docs(self, query: str, n=3) -> list[str]:
+        tokenized_query = tokenize(query)
+        scores = self.bm25.get_scores(tokenized_query)
+        print(scores)
+        top_n = np.argsort(scores)[::-1][:n]
+        print(top_n)
+        return [self.docs[i] for i in top_n]
